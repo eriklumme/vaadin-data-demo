f13e9d1ab597ebe83ff5c85b1402a97828fc1aeb
Erik Lumme <erik@vaadin.com>
2021-01-25 15:35:06 +0200

    Add Lombok

--- src/main/java/org/vaadin/erik/data/entity/Person.java
+++ src/main/java/org/vaadin/erik/data/entity/Person.java
@@ -2,12 +2,16 @@ package org.vaadin.erik.data.entity;
 
 import javax.persistence.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.vaadin.erik.data.AbstractEntity;
 import org.vaadin.erik.data.IPerson;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Person extends AbstractEntity implements IPerson {
 
     private String firstName;
@@ -17,48 +21,5 @@ public class Person extends AbstractEntity implements IPerson {
     private LocalDate dateOfBirth;
     private String occupation;
     private boolean important;
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-    public String getOccupation() {
-        return occupation;
-    }
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-    public boolean isImportant() {
-        return important;
-    }
-    public void setImportant(boolean important) {
-        this.important = important;
-    }
 
 }
